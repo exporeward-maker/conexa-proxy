033e46132e151e86e39b243844589f80a6ab7766
exporeward-maker <exporeward@gmail.com>
2026-02-05 02:24:02 +0800

    Update book-ride.js

--- api/book-ride.js
+++ api/book-ride.js
@@ -20,11 +20,5 @@ export default function handler(req, res) {
       res.status(500).json({ error: err.message });
     });
 }
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-if (req.method === "OPTIONS") {
-  return res.status(200).end();
-}
 
 
