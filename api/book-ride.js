9589a308f4833cb175e4ce62e31f257bf07a1d81
exporeward-maker <exporeward@gmail.com>
2026-02-04 23:57:40 +0800

    Update book-ride.js

--- api/book-ride.js
+++ api/book-ride.js
@@ -1,23 +1,24 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    res.status(405).json({ error: "Method not allowed" });
+    return;
   }
 
-  try {
-    const response = await fetch("https://api.conexahub.io/v1/contacts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer pit-d4b5a12b-0149-4fae-a993-9f8b27090212"
-      },
-      body: JSON.stringify(req.body)
+  fetch("https://api.conexahub.io/v1/contacts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer YOUR_CONEXA_TOKEN_HERE"
+    },
+    body: JSON.stringify(req.body)
+  })
+    .then(r => r.text())
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
     });
-
-    const result = await response.text();
-    return res.status(response.status).send(result);
-
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
 }
+
 
